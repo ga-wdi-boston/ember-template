6dbbbc5400a5cee8de7bbc19e4e25bacf3f12fd9
Jeffrey Horn <jeffh@generalassemb.ly>
2016-03-14 14:09:38 -0400

    Rename application

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-repo-template',
+    modulePrefix: 'ember-template',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
