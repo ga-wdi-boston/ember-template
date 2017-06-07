5cbdd55bb25991c08dd5abce4c065da45adae2ba
Ben Adamski <benjadamski@gmail.com>
2017-06-07 11:22:26 -0400

    Add grunt/shell.js to complete grunt deploy tasks
    - Adds grunt/shell.js file, containing the commands grunt by grunt deploy
    task.
    - This command should work regardless of the state of gh-pages, if it's
    even been created, and should exclude node_modules and bower_components
    from the cleanup step.

---
+++ grunt/shell.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const ghPagesList = ['dist/']
+
+module.exports = {
+  'git-is-clean': {
+    // `$(git status --porcelain)` will evaluate to the empty string if the
+    // working directory is clean.
+    // `test -z` will exit 0 (true) if its argument is an empty string.
+    // If it doesn't exit true, `(git status && false)` will show why the
+    // repository isn't clean and exit false causing the grunt tasks to end.
+    command: 'test -z "$(git status --porcelain)"  || (git status && false)'
+  },
+  'git-push-master': {
+    command: 'git push origin master'
+  },
+  'git-checkout-master': {
+    command: 'git checkout master'
+  },
+  'deploy-prepare': {
+    command: [
+      'git branch -D gh-pages || echo "so not removed"',
+      'git checkout --orphan gh-pages',
+      'git rm --cached \'*\'',
+      'ember build --environment=production'
+    ].join(' && ')
+  },
+  'deploy-dist': {
+    command: [
+    'git add --force dist/',
+    'git commit -m "deploy task"'
+  ].join(' && ')
+  },
+  'deploy-publish': {
+    command: [
+     'git push origin :gh-pages || echo "so not removed"',
+     'git subtree push --prefix dist origin gh-pages',
+     'git clean -x -d --force --exclude=node_modules',
+     'git checkout master'
+   ].join(' && ')
+   }
+}
